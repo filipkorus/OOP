da1f9b16be0121041c6a03414d5fd9ab13587f18
filipkorus <filipkorus123@gmail.com>
2022-10-14 23:12:34 +0200

    lab3.TestClass almost finished

--- src/lab3/TestClass.java
+++ src/lab3/TestClass.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 
 public class TestClass {
 	private final long createdAt;
+	private static int count = 0;
 	private final int id;
 
 	public static void main(String[] args) {
@@ -15,18 +16,23 @@ public class TestClass {
 		int instancesToCreate = Integer.parseInt(args[0]);
 		for (int i = 0; i < instancesToCreate; ++i) {
 			TestClass tmp = new TestClass();
-			System.out.printf("%2d. [%d]\n", i + 1, tmp.getCreatedAt());
+			System.out.printf("%2d. [%d]\n", tmp.getId(), tmp.getCreatedAt());
 		}
 
-		System.out.printf("\nLiczba obieków: %d\n", instancesToCreate);
+		System.out.printf("\nLiczba obieków: %d\n", count);
 	}
 
 	TestClass() {
-		this.id = 1;
+		++count;
+		this.id = count;
 		this.createdAt = Calendar.getInstance().getTimeInMillis();
 	}
 
 	public long getCreatedAt() {
 		return this.createdAt;
+	}
+
+	public int getId() {
+		return this.id;
 	}
 }
